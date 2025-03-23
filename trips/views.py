880ae3c742ad732dae27eb777f863514f6712b6c
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2025-03-23 12:36:42 +0300

    added utils for logsheet

--- trips/views.py
+++ trips/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from .models import Trip
 from rest_framework import viewsets
 from .serializers import TripSerializer
+from .utils import generate_log_sheet
 
 class TripViewSet(viewsets.ModelViewSet):
     queryset = Trip.objects.all()
     serializer_class = TripSerializer
 
+    def perform_create(self, serializer):
+        trip = serializer.save()
+        log_path = generate_log_sheet(trip.id)
+        trip.log_sheet = log_path
+        trip.save()
